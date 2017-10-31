078070b93a88c0a548f4788d1f0b287846412a30
单坤 <shankun@kid17.com>
2017-10-31 21:25:11 +0800

    配置命令

--- src/cli/core.js
+++ src/cli/core.js
@@ -11,14 +11,16 @@ commander
   .command('xmind-ui', 'generates ui files from xmind tree')
   .command('xmind-plan', 'generates project plan files from xmind tree')
   .command('recompose', 'use recompose faster')
-  .command('app-expand', 'expand a app busniess')
-  .command('----------------------')
-  .command('----------------------')
+  .command('app-expand', 'expand a app busniess');
+
+  console.log('----------下面是缩写的使用-------------')
+
+commander
   .command('ipt', 'alias for init-project')
   .command('icr', 'alias for init-core')
   .command('xui', 'alias for xmind-ui')
   .command('xplan', 'alias for xmind-plan')
   .command('rcp', 'alias for recompose')
-  .command('exp', 'alias for app-expand')
+  .command('exp', 'alias for app-expand');
 
 commander.parse(process.argv);
