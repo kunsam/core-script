6fa62e0cac72a3bdb8380a664c0ce27e241f8c24
单坤 <shankun@kid17.com>
2017-11-03 16:28:13 +0800

    reset

--- src/cli/init-core.js
+++ src/cli/init-core.js
@@ -6,6 +6,6 @@ const chalk = require('chalk')
 import { PROJECT_BASE_PATH, PACKAGE_BASE_PATH } from '../src.config'
 
 
-fs.copy(path.join(PACKAGE_BASE_PATH, '.core-config'), path.join(PROJECT_BASE_PATH, '.core-config'))
+fs.copy(path.join(PACKAGE_BASE_PATH, '.core-config'), path.join(PROJECT_BASE_PATH, 'core-config'))
   .then(() => console.log(chalk.yellow(`>>> 初始化成功`)))
   .catch(err => console.error(err))
