40a1478d3c1edef83c3392b3f4493925c913f412
kunsam <kunsam624@icloud.com>
2018-01-29 11:04:37 +0800

    reset

--- src/src.config.js
+++ src/src.config.js
@@ -1,4 +1,6 @@
 
+
+
 import path from 'path'
 import shell from 'shelljs'
 
@@ -9,8 +11,8 @@ export const PACKAGE_VERSION = packJson.version
 
 
 // file://Users/kunsam/Desktop/project/npm/core-script/src/project.config.js
-export const PROJECT_BASE_PATH = '/Users/kunsam/web/order-pay-system' // test
+// export const PROJECT_BASE_PATH = '/Users/kunsam/web/order-pay-system' // test
 
-// export const PROJECT_BASE_PATH = shell.pwd().stdout
+export const PROJECT_BASE_PATH = shell.pwd().stdout
 
 export const PACKAGE_BASE_PATH =  path.join(__dirname, '../')
